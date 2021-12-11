ec7f8b240a0ad9750b67fc9b9c19a4f2ab34bdbf
45gfg9 <mc45@qq.com>
2021-12-12 00:45:29 +0800

    Add references for USB functions

--- src/ATPR.cpp
+++ src/ATPR.cpp
@@ -44,12 +44,20 @@ int main() {
   }
 }
 
+// see declarations for impl notes
 usbMsgLen_t usbFunctionSetup(uint8_t data[8]) {
   // TODO
   (void)reinterpret_cast<usbRequest_t *>(data);
 
   odDebug(0x10, data, 8);
 
+  // control-in:
+  // have `usbMsgPtr` set: return size
+  // usbFunctionRead(): return USB_NO_MSG
+  //
+  // control-out:
+  // ignore incoming data: return 0
+  // usbFunctionWrite(): return USB_NO_MSG
   return USB_NO_MSG;
 }
 
@@ -60,7 +68,7 @@ uint8_t usbFunctionRead(uint8_t *data, uint8_t len) {
 
   odDebug(0x11, data, len);
 
-  // return actual bytes written
+  // return actual bytes written to `data`
   return 0;
 }
 
@@ -71,7 +79,7 @@ uint8_t usbFunctionWrite(uint8_t *data, uint8_t len) {
 
   odDebug(0x12, data, len);
 
-  // received entire payload successfully: return 1
+  // received entire payload: return 1
   // expect more data: return 0
   return 1;
 }
